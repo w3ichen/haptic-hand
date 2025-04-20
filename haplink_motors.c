a9eb68e3abd3959fdf88f83b614cfba031699406
kmarques-24 <reachkmarques@gmail.com>
2025-04-20 02:06:29 +0000

    Motors 4,5,6,7 PWM and functions added to motors.c

--- haplink_motors.c
+++ haplink_motors.c
@@ -91,14 +91,22 @@ GPIOInitTypeDef gpiomotorsinitstructure;
 TIM_TimeBaseInitTypeDef_mort  TIM_TimeBaseStructure;
 TIM_OCInitTypeDef_mort  TIM_OCInitStructure;
 
-uint16_t CCR1_Val = 0;
-uint16_t CCR2_Val = 0;
-uint16_t CCR3_Val = 0;
-uint16_t CCR4_Val = 0;
+// 1 Capture-Compare Register per timer channel - renamed to be timer-specific
+uint16_t TIM3_CCR1_Val = 0;
+uint16_t TIM3_CCR2_Val = 0;
+uint16_t TIM3_CCR3_Val = 0;
+uint16_t TIM3_CCR4_Val = 0;
+uint16_t TIM4_CCR1_Val = 0;
+uint16_t TIM4_CCR2_Val = 0;
+uint16_t TIM4_CCR3_Val = 0;
 
 double dutyPrint1 = 0;
 double dutyPrint2 = 0;
 double dutyPrint3 = 0;
+double dutyPrint4 = 0;
+double dutyPrint5 = 0;
+double dutyPrint6 = 0;
+double dutyPrint7 = 0;
 
 void initMotorsGpio( void )
 {
@@ -109,20 +117,19 @@ void initMotorsGpio( void )
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
 
-
     // Init PWM pins
     gpiomotorsinitstructure.GPIO_Mode = GPIOModeAF;
     gpiomotorsinitstructure.GPIO_OType = GPIOOTypePP;
     gpiomotorsinitstructure.GPIO_Speed = GPIOHighSpeed;
     gpiomotorsinitstructure.GPIO_PuPd = GPIOPuPdUP;
-
+    // Delta
     gpiomotorsinitstructure.GPIO_Pin = GPIOPin6;
     GPIOInit((GPIOTypeDef *)GPIOC_BASE_MORT, &gpiomotorsinitstructure); //PWM1
     gpiomotorsinitstructure.GPIO_Pin = GPIOPin7;
     GPIOInit((GPIOTypeDef *)GPIOC_BASE_MORT, &gpiomotorsinitstructure); //PWM2
     gpiomotorsinitstructure.GPIO_Pin = GPIOPin8;
     GPIOInit((GPIOTypeDef *)GPIOC_BASE_MORT, &gpiomotorsinitstructure); //PWM3
-    
+    // Fingers
     gpiomotorsinitstructure.GPIO_Pin = GPIOPin9;
     GPIOInit((GPIOTypeDef *)GPIOC_BASE_MORT, &gpiomotorsinitstructure); //PWM4
     gpiomotorsinitstructure.GPIO_Pin = GPIOPin12;
@@ -132,23 +139,23 @@ void initMotorsGpio( void )
     gpiomotorsinitstructure.GPIO_Pin = GPIOPin14;
     GPIOInit((GPIOTypeDef *)GPIOD_BASE_MORT, &gpiomotorsinitstructure); //PWM7
 
-    /* Connect TIM3 pins to AF2 */  
-        // should all of these be on tim3? explain for Kiran
+    /* Connect TIM3 and TIM4 pins to AF2 */  
+    // Delta
     GPIOPinAFConfig((GPIOTypeDef *)GPIOC_BASE_MORT, GPIOPinSource6, GPIO_AF_TIM3);
     GPIOPinAFConfig((GPIOTypeDef *)GPIOC_BASE_MORT, GPIOPinSource7, GPIO_AF_TIM3); 
     GPIOPinAFConfig((GPIOTypeDef *)GPIOC_BASE_MORT, GPIOPinSource8, GPIO_AF_TIM3);
+    // Fingers
     GPIOPinAFConfig((GPIOTypeDef *)GPIOC_BASE_MORT, GPIOPinSource9, GPIO_AF_TIM3); 
-    GPIOPinAFConfig((GPIOTypeDef *)GPIOD_BASE_MORT, GPIOPinSource12, GPIO_AF_TIM3); 
-    GPIOPinAFConfig((GPIOTypeDef *)GPIOD_BASE_MORT, GPIOPinSource13, GPIO_AF_TIM3); 
-    GPIOPinAFConfig((GPIOTypeDef *)GPIOD_BASE_MORT, GPIOPinSource14, GPIO_AF_TIM3); 
-
+    GPIOPinAFConfig((GPIOTypeDef *)GPIOD_BASE_MORT, GPIOPinSource12, GPIO_AF_TIM4); 
+    GPIOPinAFConfig((GPIOTypeDef *)GPIOD_BASE_MORT, GPIOPinSource13, GPIO_AF_TIM4); 
+    GPIOPinAFConfig((GPIOTypeDef *)GPIOD_BASE_MORT, GPIOPinSource14, GPIO_AF_TIM4); 
 
     /*Then the pins used for direction*/
     gpiomotorsinitstructure.GPIO_Mode = GPIOModeOUT;
     gpiomotorsinitstructure.GPIO_OType = GPIOOTypePP;
     gpiomotorsinitstructure.GPIO_Speed = GPIOHighSpeed;
     gpiomotorsinitstructure.GPIO_PuPd = GPIOPuPdNOPULL;
-
+    // Delta
     gpiomotorsinitstructure.GPIO_Pin = GPIOPin10;
     GPIOInit((GPIOTypeDef *)GPIOC_BASE_MORT, &gpiomotorsinitstructure); // C10
     gpiomotorsinitstructure.GPIO_Pin = GPIOPin11;
@@ -161,7 +168,7 @@ void initMotorsGpio( void )
     GPIOInit((GPIOTypeDef *)GPIOD_BASE_MORT, &gpiomotorsinitstructure); // D0
     gpiomotorsinitstructure.GPIO_Pin = GPIOPin1;
     GPIOInit((GPIOTypeDef *)GPIOD_BASE_MORT, &gpiomotorsinitstructure); // D1
-
+    // Fingers
     gpiomotorsinitstructure.GPIO_Pin = GPIOPin3;
     GPIOInit((GPIOTypeDef *)GPIOD_BASE_MORT, &gpiomotorsinitstructure); // D3
     gpiomotorsinitstructure.GPIO_Pin = GPIOPin4;
@@ -183,10 +190,11 @@ void initMotorsGpio( void )
 void initHaplinkMotors( void )
 {
     uint16_t PrescalerValue = 4;
-    CCR1_Val = 0;
-    CCR2_Val = 0;
-    CCR3_Val = 0;
-    CCR4_Val = 0;
+    TIM3_CCR1_Val = 0;
+    TIM3_CCR2_Val = 0;
+    TIM3_CCR3_Val = 0;
+    TIM3_CCR4_Val = 0;
+
     /* TIM3 clock enable */
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
   
@@ -202,55 +210,66 @@ void initHaplinkMotors( void )
     TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up_MORT;
     TIM_TimeBaseInit_mort(TIM3_MORT, &TIM_TimeBaseStructure);
 
-    /* PWM1 Mode configuration: Channel1 */
-    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1_MORT;
-    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable_MORT;
-    TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
-    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High_MORT;
-
+    // Delta
+    /* PWM1 Mode configuration: TIM3 Channel1 */
+        // PWM Mode is either 1 or 2 (mode is unrelated to motor number). See Section 17.3.9
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1_MORT;              // same for rest
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable_MORT;  // same for rest (removed redundant line)
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High_MORT;      // same for rest
+    TIM_OCInitStructure.TIM_Pulse = TIM3_CCR1_Val;                      // CCR changes for each
     TIM_OC1Init_mort(TIM3_MORT, &TIM_OCInitStructure);
-
     TIM_OC1PreloadConfig_mort(TIM3_MORT, TIM_OCPreload_Enable_MORT);
 
-    /* PWM1 Mode configuration: Channel2 */
-    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable_MORT;
-    TIM_OCInitStructure.TIM_Pulse = CCR2_Val;
-
+    /* PWM1 Mode configuration: TIM3 Channel2 */
+    TIM_OCInitStructure.TIM_Pulse = TIM3_CCR2_Val;
     TIM_OC2Init_mort(TIM3_MORT, &TIM_OCInitStructure);
-
     TIM_OC2PreloadConfig_mort(TIM3_MORT, TIM_OCPreload_Enable_MORT);
 
-    /* PWM1 Mode configuration: Channel3 */
-    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable_MORT;
-    TIM_OCInitStructure.TIM_Pulse = CCR3_Val;
-
+    /* PWM1 Mode configuration: TIM3 Channel3 */
+    TIM_OCInitStructure.TIM_Pulse = TIM3_CCR3_Val;
     TIM_OC3Init_mort(TIM3_MORT, &TIM_OCInitStructure);
-
     TIM_OC3PreloadConfig_mort(TIM3_MORT, TIM_OCPreload_Enable_MORT);
 
-    /* PWM1 Mode configuration: Channel4 */
-    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable_MORT;
-    TIM_OCInitStructure.TIM_Pulse = CCR4_Val;
-
+    // Fingers
+    /* PWM1 Mode configuration: TIM3 Channel4 */
+    TIM_OCInitStructure.TIM_Pulse = TIM3_CCR4_Val;
     TIM_OC4Init_mort(TIM3_MORT, &TIM_OCInitStructure);
-
     TIM_OC4PreloadConfig_mort(TIM3_MORT, TIM_OCPreload_Enable_MORT);
 
-    TIM_ARRPreloadConfig_mort(TIM3_MORT, ENABLE);
+    /* PWM1 Mode configuration: TIM4 Channel1 */
+    TIM_OCInitStructure.TIM_Pulse = TIM4_CCR1_Val;
+    TIM_OC1Init_mort(TIM4_MORT, &TIM_OCInitStructure);
+    TIM_OC1PreloadConfig_mort(TIM4_MORT, TIM_OCPreload_Enable_MORT);
 
-    /* TIM3 enable counter */
+    /* PWM1 Mode configuration: TIM4 Channel2 */
+    TIM_OCInitStructure.TIM_Pulse = TIM4_CCR2_Val;
+    TIM_OC2Init_mort(TIM4_MORT, &TIM_OCInitStructure);
+    TIM_OC2PreloadConfig_mort(TIM4_MORT, TIM_OCPreload_Enable_MORT);
+
+    /* PWM1 Mode configuration: TIM4 Channel3 */
+    TIM_OCInitStructure.TIM_Pulse = TIM4_CCR3_Val;
+    TIM_OC3Init_mort(TIM4_MORT, &TIM_OCInitStructure);
+    TIM_OC3PreloadConfig_mort(TIM4_MORT, TIM_OCPreload_Enable_MORT);
+
+    TIM_ARRPreloadConfig_mort(TIM3_MORT, ENABLE);
+    TIM_ARRPreloadConfig_mort(TIM4_MORT, ENABLE);
+
+    /* TIM3 and TIM4 enable counter */
     TIM_Cmd_mort(TIM3_MORT, ENABLE);  
+    TIM_Cmd_mort(TIM4_MORT, ENABLE);
 }
 
 
 void turnOffMotorsDirPins( void )
 {
+    // Delta
     GPIOClearBits(PWM11_PORT, (uint16_t)PWM11_PIN );
     GPIOClearBits(PWM12_PORT, (uint16_t)PWM12_PIN );
     GPIOClearBits(PWM21_PORT, (uint16_t)PWM21_PIN );
     GPIOClearBits(PWM22_PORT, (uint16_t)PWM22_PIN );
     GPIOClearBits(PWM31_PORT, (uint16_t)PWM31_PIN );
     GPIOClearBits(PWM32_PORT, (uint16_t)PWM32_PIN );
+    // Fingers
     GPIOClearBits(PWM41_PORT, (uint16_t)PWM41_PIN );
     GPIOClearBits(PWM42_PORT, (uint16_t)PWM42_PIN );
     GPIOClearBits(PWM51_PORT, (uint16_t)PWM51_PIN );
@@ -298,7 +317,53 @@ void motor3Dir2 (void)
     GPIOSetBits(PWM32_PORT, (uint16_t)PWM32_PIN );
 }
 
+void motor4Dir1 (void)
+{
+    GPIOSetBits(PWM41_PORT, (uint16_t)PWM41_PIN );
+    GPIOClearBits(PWM42_PORT, (uint16_t)PWM42_PIN );
+}
 
+void motor4Dir2 (void)
+{
+    GPIOClearBits(PWM41_PORT, (uint16_t)PWM41_PIN );
+    GPIOSetBits(PWM42_PORT, (uint16_t)PWM42_PIN );
+}
+
+void motor5Dir1 (void)
+{
+    GPIOSetBits(PWM51_PORT, (uint16_t)PWM51_PIN );
+    GPIOClearBits(PWM52_PORT, (uint16_t)PWM52_PIN );
+}
+
+void motor5Dir2 (void)
+{
+    GPIOClearBits(PWM51_PORT, (uint16_t)PWM51_PIN );
+    GPIOSetBits(PWM52_PORT, (uint16_t)PWM52_PIN );
+}
+
+void motor6Dir1 (void)
+{
+    GPIOSetBits(PWM61_PORT, (uint16_t)PWM61_PIN );
+    GPIOClearBits(PWM62_PORT, (uint16_t)PWM62_PIN );
+}
+
+void motor6Dir2 (void)
+{
+    GPIOClearBits(PWM61_PORT, (uint16_t)PWM61_PIN );
+    GPIOSetBits(PWM62_PORT, (uint16_t)PWM62_PIN );
+}
+
+void motor7Dir1 (void)
+{
+    GPIOSetBits(PWM71_PORT, (uint16_t)PWM71_PIN );
+    GPIOClearBits(PWM72_PORT, (uint16_t)PWM72_PIN );
+}
+
+void motor7Dir2 (void)
+{
+    GPIOClearBits(PWM71_PORT, (uint16_t)PWM71_PIN );
+    GPIOSetBits(PWM72_PORT, (uint16_t)PWM72_PIN );
+}
 
 /*--Functions to directly set the new PWM duty cycle--------------------------*/
 
@@ -371,6 +436,60 @@ void updateDutyCycle4( double newDuty )
     TIM_SetCompare4_mort(TIM3_MORT, newValue);  
 }
 
+/**************************************
+ newDuty should be a value betwen 0 and 1
+***************************************/
+void updateDutyCycle5( double newDuty )
+{
+    uint32_t newValue;
+    if (newDuty > 1)
+    {
+        newDuty = 1;
+    }
+    else if (newDuty < 0 )
+    {
+        newDuty = 0;
+    }
+    newValue = (uint32_t)(newDuty*PERIOD_PWM);
+    TIM_SetCompare1_mort(TIM4_MORT, newValue);  
+}
+
+/**************************************
+ newDuty should be a value betwen 0 and 1
+***************************************/
+void updateDutyCycle6( double newDuty )
+{
+    uint32_t newValue;
+    if (newDuty > 1)
+    {
+        newDuty = 1;
+    }
+    else if (newDuty < 0 )
+    {
+        newDuty = 0;
+    }
+    newValue = (uint32_t)(newDuty*PERIOD_PWM);
+    TIM_SetCompare2_mort(TIM4_MORT, newValue);  
+}
+
+/**************************************
+ newDuty should be a value betwen 0 and 1
+***************************************/
+void updateDutyCycle7( double newDuty )
+{
+    uint32_t newValue;
+    if (newDuty > 1)
+    {
+        newDuty = 1;
+    }
+    else if (newDuty < 0 )
+    {
+        newDuty = 0;
+    }
+    newValue = (uint32_t)(newDuty*PERIOD_PWM);
+    TIM_SetCompare3_mort(TIM4_MORT, newValue);  
+}
+
 /*--Debugging Functions-------------------------------------------------------*/
 void outputDutyCycleMotor1(double duty)
 {
@@ -413,6 +532,66 @@ void outputDutyCycleMotor3(double duty)
     }
     dutyPrint3 = duty;
     updateDutyCycle3(duty); 
+}
+
+void outputDutyCycleMotor4(double duty)
+{
+    if (duty < 0)
+    {
+        motor4Dir1();
+        duty = duty*(-1.0);
+    }
+    else
+    {
+        motor4Dir2();
+    }
+    dutyPrint4 = duty;
+    updateDutyCycle4(duty); 
+}
+
+void outputDutyCycleMotor5(double duty)
+{
+    if (duty < 0)
+    {
+        motor5Dir1();
+        duty = duty*(-1.0);
+    }
+    else
+    {
+        motor5Dir2();
+    }
+    dutyPrint5 = duty;
+    updateDutyCycle5(duty); 
+}
+
+void outputDutyCycleMotor6(double duty)
+{
+    if (duty < 0)
+    {
+        motor6Dir1();
+        duty = duty*(-1.0);
+    }
+    else
+    {
+        motor6Dir2();
+    }
+    dutyPrint6 = duty;
+    updateDutyCycle6(duty); 
+}
+
+void outputDutyCycleMotor7(double duty)
+{
+    if (duty < 0)
+    {
+        motor7Dir1();
+        duty = duty*(-1.0);
+    }
+    else
+    {
+        motor7Dir2();
+    }
+    dutyPrint7 = duty;
+    updateDutyCycle7(duty); 
 }
 /**/
 /*******************************************************************************
@@ -466,8 +645,8 @@ void outputTorqueMotor2(double torque)
 /*******************************************************************************
   * @name   outputTorqueMotor3
   * @brief  Converts the desired torque to duty cycle based on calibration and
-  *         outputs it on Motor2.
-  * @param  double torque: the torque output desired on Motor 2 in Nm.
+  *         outputs it on Motor3.
+  * @param  double torque: the torque output desired on Motor 3 in Nm.
   * @retval None.
   */
 void outputTorqueMotor3(double torque)
@@ -486,4 +665,104 @@ void outputTorqueMotor3(double torque)
     duty = torque*65.13; //replace the 65.13 with value for specific motor
     dutyPrint3 = duty;
     updateDutyCycle3(duty);
+}
+
+/*******************************************************************************
+  * @name   outputTorqueMotor4
+  * @brief  Converts the desired torque to duty cycle based on calibration and
+  *         outputs it on Motor4.
+  * @param  double torque: the torque output desired on Motor 4 in Nm.
+  * @retval None.
+  */
+void outputTorqueMotor4(double torque)
+{
+    double duty;
+    if (torque < 0)
+    {
+        motor4Dir1();
+        torque = torque*(-1.0);
+    }
+    else
+    {
+        motor4Dir2();
+    }
+
+    duty = torque*65.13; //replace the 65.13 with value for specific motor
+    dutyPrint4 = duty;
+    updateDutyCycle4(duty);
+}
+
+/*******************************************************************************
+  * @name   outputTorqueMotor5
+  * @brief  Converts the desired torque to duty cycle based on calibration and
+  *         outputs it on Motor5.
+  * @param  double torque: the torque output desired on Motor 5 in Nm.
+  * @retval None.
+  */
+void outputTorqueMotor5(double torque)
+{
+    double duty;
+    if (torque < 0)
+    {
+        motor5Dir1();
+        torque = torque*(-1.0);
+    }
+    else
+    {
+        motor5Dir2();
+    }
+
+    duty = torque*65.13; //replace the 65.13 with value for specific motor
+    dutyPrint5 = duty;
+    updateDutyCycle5(duty);
+}
+
+/*******************************************************************************
+  * @name   outputTorqueMotor6
+  * @brief  Converts the desired torque to duty cycle based on calibration and
+  *         outputs it on Motor6.
+  * @param  double torque: the torque output desired on Motor 6 in Nm.
+  * @retval None.
+  */
+void outputTorqueMotor6(double torque)
+{
+    double duty;
+    if (torque < 0)
+    {
+        motor6Dir1();
+        torque = torque*(-1.0);
+    }
+    else
+    {
+        motor6Dir2();
+    }
+
+    duty = torque*65.13; //replace the 65.13 with value for specific motor
+    dutyPrint6 = duty;
+    updateDutyCycle6(duty);
+}
+
+/*******************************************************************************
+  * @name   outputTorqueMotor7
+  * @brief  Converts the desired torque to duty cycle based on calibration and
+  *         outputs it on Motor7.
+  * @param  double torque: the torque output desired on Motor 7 in Nm.
+  * @retval None.
+  */
+void outputTorqueMotor7(double torque)
+{
+    double duty;
+    if (torque < 0)
+    {
+        motor7Dir1();
+        torque = torque*(-1.0);
+    }
+    else
+    {
+        motor7Dir2();
+    }
+
+    duty = torque*65.13; //replace the 65.13 with value for specific motor
+    dutyPrint7 = duty;
+    updateDutyCycle7(duty);
 }
